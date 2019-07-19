7205be8275b7279da6625585aaf6b9f09a4fb942
ejfrm <ejfrm@DESKTOP-QV5Q5AA>
2019-07-19 11:32:00 +0900

    yong : 완성

--- Namu/src/main/java/com/youlove/web/planner/PlannerRestController.java
+++ Namu/src/main/java/com/youlove/web/planner/PlannerRestController.java
@@ -34,46 +34,111 @@ public class PlannerRestController {
 	@Qualifier("plannerServiceImpl")
 	private PlannerService plannerService;
 	
-	@RequestMapping( value="getRouteList") 
-	public  List<Map<String, Object>> getRouteList() throws Exception{
+//	@RequestMapping( value="getRouteList") 
+//	public  List<Map<String, Object>> getRouteList() throws Exception{
+//	
+//	System.out.println("PlannerRestController--------getRouteList");
+//	 List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
+//       List rlist = new ArrayList();
+//       String title = "";
+//       Date start;
+//       Date end;
+//       String color = "";
+//  
+//       rlist =  plannerService.getRouteList(1);
+//          for(int i=0; i<rlist.size(); i++){   
+//             Map<String, Object> map = new HashMap<String, Object>();
+//             title = ((Route) rlist.get(i)).getCityName();   
+//             start = ((Route) rlist.get(i)).getStartDate();
+//             end = ((Route) rlist.get(i)).getEndDate();
+//             int cityOrder=((Route) rlist.get(i)).getCityOrder();
+//             
+//             //hard coding/...... 방법 알아보기.
+//             if(cityOrder==1){color="#CCCC66";}
+//             if(cityOrder==2){color="#66CCFF";}
+//             if(cityOrder==3){color="#9999FF";}
+//             if(cityOrder==4){color="#D3FFCE";}
+//             if(cityOrder==5){color="#FFC7C6";}
+//             if(cityOrder==6){color="#98DDDE";}
+//             if(cityOrder==7){color="#FFD954";}
+//             if(cityOrder==8){color="#003A70";}
+//             if(cityOrder==9){color="#FFF38C";}
+//             if(cityOrder==10){color="#F08080";}
+//             map.put("title", title);
+//             map.put("start", start);
+//             map.put("end", end);
+//             map.put("color", color);
+//             map.put("id",cityOrder);
+//             result.add(map);           
+//           }
+//  return result;
+// }
+//		
 	
-	System.out.println("PlannerRestController--------getRouteList");
+	@RequestMapping( value="getScheduleList") 
+	public  List<Map<String, Object>> getScheduleList() throws Exception{
+	
+	System.out.println("PlannerRestController--------getScheduleList");
 	 List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
-       List rlist = new ArrayList();
-       String title = "";
-       Date start;
-       Date end;
-       String color = "";
-  
-       rlist =  plannerService.getRouteList(1);
-          for(int i=0; i<rlist.size(); i++){   
-             Map<String, Object> map = new HashMap<String, Object>();
-             title = ((Route) rlist.get(i)).getCityName();   
-             start = ((Route) rlist.get(i)).getStartDate();
-             end = ((Route) rlist.get(i)).getEndDate();
-             int cityOrder=((Route) rlist.get(i)).getCityOrder();
+	 List rlist = new ArrayList();
+	 List slist = new ArrayList();
+    
+     String title = "";
+     Date start;
+     Date end;
+     String color = "";
+      
+     rlist =  plannerService.getRouteList(1);
+     for(int i=0; i<rlist.size(); i++){   
+        Map<String, Object> map = new HashMap<String, Object>();
+        title = ((Route) rlist.get(i)).getCityName();   
+        start = ((Route) rlist.get(i)).getStartDate();
+        end = ((Route) rlist.get(i)).getEndDate();
+        int cityOrder=((Route) rlist.get(i)).getCityOrder();
+        
+        //hard coding/...... 방법 알아보기.
+        if(cityOrder==1){color="#CCCC66";}
+        if(cityOrder==2){color="#66CCFF";}
+        if(cityOrder==3){color="#9999FF";}
+        if(cityOrder==4){color="#D3FFCE";}
+        if(cityOrder==5){color="#FFC7C6";}
+        if(cityOrder==6){color="#98DDDE";}
+        if(cityOrder==7){color="#FFD954";}
+        if(cityOrder==8){color="#003A70";}
+        if(cityOrder==9){color="#FFF38C";}
+        if(cityOrder==10){color="#F08080";}
+        map.put("title", title);
+        map.put("start", start);
+        map.put("end", end);
+        map.put("color", color);
+        map.put("id",cityOrder);
+        result.add(map);           
+      }
+       
+       slist =  plannerService.getScheduleList(1);
+          for(int j=0; j<slist.size(); j++){   
+
+             Map<String, Object> map2 = new HashMap<String, Object>();
+             title = ((Schedule) slist.get(j)).getScheName();   
+             start = ((Schedule) slist.get(j)).getScheDay();
+             int scheCode=((Schedule) slist.get(j)).getScheCode();
+             color= ((Schedule) slist.get(j)).getColor();   
              
-             //hard coding/...... 방법 알아보기.
-             if(cityOrder==1){color="#CCCC66";}
-             if(cityOrder==2){color="#66CCFF";}
-             if(cityOrder==3){color="#9999FF";}
-             if(cityOrder==4){color="#D3FFCE";}
-             if(cityOrder==5){color="#FFC7C6";}
-             if(cityOrder==6){color="#98DDDE";}
-             if(cityOrder==7){color="#FFD954";}
-             if(cityOrder==8){color="#003A70";}
-             if(cityOrder==9){color="#FFF38C";}
-             if(cityOrder==10){color="#F08080";}
-             map.put("title", title);
-             map.put("start", start);
-             map.put("end", end);
-             map.put("color", color);
-             map.put("id",cityOrder);
-             result.add(map);           
+             map2.put("title", title);
+             map2.put("start", start);
+             map2.put("id",scheCode);
+             map2.put("color", color);
+             result.add(map2);  
+             
            }
+          
+        
   return result;
  }
-		
+	
+	
+	
+	
 		// 일정등록
 //		@RequestMapping(value = "/planner/addSchedule", method = RequestMethod.POST)
 //		public HashMap<String, Object> addSchedule(@RequestBody HashMap<String, Object> requestParam){
@@ -92,7 +157,7 @@ public class PlannerRestController {
 
 
 	@RequestMapping( value="json/getSchedule/{scheCode}", method=RequestMethod.GET )
-	public com.youlove.service.domain.Schedule getSchedule( @PathVariable int scheCode) throws Exception{
+	public Schedule getSchedule( @PathVariable int scheCode) throws Exception{
 		
 		System.out.println("/planner/json/getSchedule: GET");
 	
