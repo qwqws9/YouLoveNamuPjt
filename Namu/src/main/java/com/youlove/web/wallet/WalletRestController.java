ce6792fcc1b2858ac82ae70909c99a5e555f84b4
KWON <KWON@KWONS-NOTEBOOK>
2019-07-29 10:34:37 +0900

    kwon : 다시 받아야지

--- Namu/src/main/java/com/youlove/web/wallet/WalletRestController.java
+++ Namu/src/main/java/com/youlove/web/wallet/WalletRestController.java
@@ -104,27 +104,6 @@ public class WalletRestController {
 		
 	}
 	
-	@RequestMapping(value="json/updateWallet", method=RequestMethod.POST)
-	public boolean updateWallet(@ModelAttribute("wallet") Wallet wallet, MultipartFile file, HttpServletRequest request) throws Exception{
-		
-		System.out.println("/wallet/json/updateWallet :: POST");
-		
-		walletService.getWallet(wallet.getWalletDetailCode());
-		
-		if(!file.isEmpty() && file != null){
-			String fileName = FileNameUUId.convert(file, "wallet", request);
-			
-			wallet.setWalletImage(fileName);
-		}
-
-		System.out.println(wallet);
-		
-		//walletService.updateWallet(wallet);
-		
-		return true;
-		
-	}
-	
 	@RequestMapping(value = "/json/getWallet/{walletDetailCode}", method=RequestMethod.GET)
 	public Wallet getWallet(@PathVariable int walletDetailCode) throws Exception{
 		
@@ -134,6 +113,29 @@ public class WalletRestController {
 		System.out.println(wallet);
 		
 		return wallet;
+		
+	}
+	
+	@RequestMapping(value="json/updateWallet", method=RequestMethod.POST)
+	public boolean updateWallet(@ModelAttribute("wallet") Wallet wallet, MultipartFile file, HttpServletRequest request) throws Exception{
+		
+		System.out.println("/wallet/json/updateWallet :: POST");
+		
+		if(!file.isEmpty() && file != null){
+			String fileName = FileNameUUId.convert(file, "wallet", request);
+			
+			wallet.setWalletImage(fileName);
+		}else{
+			Wallet walletOriginal = walletService.getWallet(wallet.getWalletDetailCode());
+			
+			wallet.setWalletImage(walletOriginal.getWalletImage());
+		}
+
+		System.out.println(wallet);
+		
+		walletService.updateWallet(wallet);
+		
+		return true;
 		
 	}
 	
